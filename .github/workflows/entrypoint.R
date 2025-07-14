25ef7e75e6926c0695f87331ebe476f264af749b
Praveen Morais <praveen.morais13@gmail.com>
2025-07-14 17:46:15 +0530

    Update entrypoint.R

--- .github/workflows/entrypoint.R
+++ .github/workflows/entrypoint.R
@@ -1,9 +1,13 @@
 #!/usr/bin/env Rscript
 
-rsconnect::setAccountInfo(name = Sys.getenv("SHINY_USERNAME"),
-                          token = Sys.getenv("SHINY_TOKEN"),
-                          secret = Sys.getenv("SHINY_SECRET"))
+rsconnect::setAccountInfo(
+  name = Sys.getenv("SHINY_USERNAME"),
+  token = Sys.getenv("SHINY_TOKEN"),
+  secret = Sys.getenv("SHINY_SECRET")
+)
 
-rsconnect::deployApp(appName = Sys.getenv("APP_NAME"),
-                     appDir = Sys.getenv("APP_DIR"),
-                     launch.browser = FALSE)
+rsconnect::deployApp(
+  appDir = Sys.getenv("APP_DIR"),
+  appName = Sys.getenv("APP_NAME"),
+  launch.browser = FALSE
+)
